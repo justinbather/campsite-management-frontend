a6bc863814b8cd0ca1550d21ee204ffd2ee57abb
justinbather <justinbather@hotmail.com>
2023-09-05 16:27:34 -0400

    added env var for s3 bucket

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
 export const STRIPE_KEY = process.env.STRIPE_KEY
 export const API_URL = 'https://campsite-management-api-27012b8fbddf.herokuapp.com'
-export const MEDIA_URL = 'https://campsite-management-api-27012b8fbddf.herokuapp.com/app'
+export const MEDIA_URL = process.env.REACT_APP_AWS_S3_URL
 export const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY
