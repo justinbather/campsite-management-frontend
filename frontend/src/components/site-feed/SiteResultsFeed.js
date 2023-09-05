35684df0656a85f44e494a651e41fc670544c9f9
Cooper <cooper.macgregor14@gmail.com>
2023-09-05 18:12:31 -0400

    styling

--- frontend/src/components/site-feed/SiteResultsFeed.js
+++ frontend/src/components/site-feed/SiteResultsFeed.js
@@ -10,8 +10,6 @@ const SiteResultsFeed = (props) => {
   const [selectedDeparture, setSelectedDeparture] = useState([]);
   const { parkId } = useParams();
 
-  
-
   const navigate = useNavigate();
   const handleClick = (siteId) => {
     navigate(
@@ -26,27 +24,26 @@ const SiteResultsFeed = (props) => {
   }, [props]);
 
   return (
-    
     <div className="flex flex-nowrap ml-10">
       <h1 className="absolute left-36 text-center font-inter text-4xl font-bold">
         {props.feedTitle}
       </h1>
-        <div className="flex justify-center px-20 py-14 gap-5">
-          {props.sites.map((site) => (
-            <div className="flex w-[20rem] h-[20rem] bg-base-100 drop-shadow-xl justify-center overflow-hidden rounded-xl hover:ring-4 ring-neutral-300">
-              <img
-                className="w-full h-full"
-                alt="image"
-                src={site.thumbnail}
-                role="button"
-                key={site.id}
-                onClick={() => handleClick(site.id)}
-              />
-              <h1 className="text-lg text-center text-base-100 absolute bottom-0">
-                Site {site.id} - ${site.price}
-              </h1>
+      <div className="flex justify-center px-20 py-14 gap-5">
+        {props.sites.map((site) => (
+          <div className="flex w-[25rem] h-[20rem] bg-base-100 drop-shadow-xl justify-center overflow-hidden rounded-xl hover:ring-4 ring-neutral-300">
+            <img
+              className="w-full h-full"
+              alt="image"
+              src={site.thumbnail}
+              role="button"
+              key={site.id}
+              onClick={() => handleClick(site.id)}
+            />
+            <h1 className="text-lg text-center text-base-100 absolute bottom-0">
+              Site {site.id} - ${site.price}
+            </h1>
           </div>
-          ))}
+        ))}
       </div>
     </div>
   );
